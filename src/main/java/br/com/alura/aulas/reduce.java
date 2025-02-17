b675e858390502f1d310c2548176c43faa4e644b
Mathew <matheoshanna10@gmail.com>
2025-02-16 22:36:13 -0300

    feat: aprendendo reduce

--- src/main/java/br/com/alura/aulas/reduce.java
+++ src/main/java/br/com/alura/aulas/reduce.java
@@ -2,14 +2,17 @@ package br.com.alura.aulas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class reduce {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-//        list.stream()
-//                .reduce(accum)
+        Optional<Integer> reduce = list.stream()
+                .reduce((n1,n2) -> n1 + n2);
+
+        System.out.println(reduce.get());
 
 
     }
